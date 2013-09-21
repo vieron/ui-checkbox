1f5ac8b9a18df6c770a71699ded969e978ec9345
vieron <javiersanchezmarin@gmail.com>
2013-09-22 00:26:10 +0200

    more tests

--- tests/spec/ui-checkbox.js
+++ tests/spec/ui-checkbox.js
@@ -22,22 +22,44 @@ describe('ui-checkbox', function() {
     });
 
 
-    describe('#isChecked()', function(){
+    describe('#isChecked()', function() {
         it('should be checked', function() {
             expect(this.checkbox.isChecked()).to.be.true;
         });
     });
 
+    describe('#val()', function() {
+        it('should be checked', function() {
+            expect(this.checkbox.val()).to.be.true;
+        });
+    });
 
-    describe('#uncheck()', function(){
+
+    describe('#uncheck()', function() {
         it('should be unchecked', function() {
             this.checkbox.uncheck();
             expect(this.checkbox.isChecked()).to.be.false;
         });
+    });
 
+    describe('#check()', function() {
         it('should be checked', function() {
             this.checkbox.check();
             expect(this.checkbox.isChecked()).to.be.true;
+        });
+    });
+
+    describe('#toggle()', function() {
+        it('should be unchecked', function() {
+            this.checkbox.toggle();
+            expect(this.checkbox.isChecked()).to.be.false;
+        });
+    });
+
+    describe('#name() / #name(string)', function() {
+        it('should return the name setted', function() {
+            this.checkbox.name('settings');
+            expect(this.checkbox.name()).to.equal('settings');
         });
     });
 
